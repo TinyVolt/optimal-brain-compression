d403933419ad6f4f6c2165fb20da7a6e3cf1b4d8
vinsis <sisodia.vinay@gmail.com>
2023-12-10 22:08:34 +0800

    added types

---
+++ src/_types.py
@@ -0,0 +1,18 @@
+import torch
+from pydantic import BaseModel
+from typing import Tuple
+
+# Output of `_utils.find_optimal_scales_and_zeros`
+# `scales` and `rounded_zeros` are tensors of shape `(n_rows,)`
+class ScalesAndZeros(BaseModel):
+    scales:torch.Tensor
+    rounded_zeros:torch.Tensor
+
+# Output of `exact_obc._handle_zeros`
+Weights_Hessian_Tuple = Tuple[torch.Tensor, torch.Tensor]
+
+# Output for quantized weights
+class QuantizedMatrix(BaseModel):
+    quantized_matrix:torch.Tensor
+    scales:torch.Tensor
+    rounded_zeros:torch.Tensor
