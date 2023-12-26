bcb776da3f2a4f88073905cbd1bb9df94c09b1d3
vinsis <sisodia.vinay@gmail.com>
2023-12-26 11:12:33 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+
+import pkg_resources
+from setuptools import setup, find_packages
+
+setup(
+    name="optimal-brain-compression",
+    py_modules=["optimal_brain_compression"],
+    version="0.1",
+    description="Unofficial implementation of the Optimal Brain Compression algorithm",
+    author="Vinay Sisodia",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=["torch"],
+    include_package_data=True,
+)
